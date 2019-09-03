11aa4776abab7dafac4a9ffabeab6964de9b4dbf
Kmaslowiec <kmaslowiec@gmail.com>
2019-09-04 00:16:04 +0200

    Gui of JList added

--- src/main/java/com/github/kmaslowiec/template_manager/views/MainFrame.java
+++ src/main/java/com/github/kmaslowiec/template_manager/views/MainFrame.java
@@ -1,5 +1,6 @@
 package com.github.kmaslowiec.template_manager.views;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -9,9 +10,15 @@ import com.github.kmaslowiec.template_manager.common.OpenFile;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JTextPane;
+import javax.swing.JList;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JScrollPane;
+import javax.swing.AbstractListModel;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
@@ -19,7 +26,8 @@ public class MainFrame extends JFrame {
 	private JMenu mnFile;
 	private JMenuItem mntmAddTemplate;
 	private OpenFile openFile = new OpenFile();
-	private JTextPane textPane;
+	private JScrollPane scrollPane;
+	private JList list;
 
 	/**
 	 * Launch the application.
@@ -59,22 +67,41 @@ public class MainFrame extends JFrame {
 		
 		mnFile.add(mntmAddTemplate);
 		
-		textPane = new JTextPane();
+		scrollPane = new JScrollPane();
+		
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE)
-					.addContainerGap())
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 425, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(13, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(textPane, GroupLayout.DEFAULT_SIZE, 225, Short.MAX_VALUE)
-					.addContainerGap())
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 227, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
+		
+		list = new JList();
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Konrad", "John", "Test"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		scrollPane.setViewportView(list);
+		list.addListSelectionListener(a->{
+			if(!a.getValueIsAdjusting()) {
+				System.out.println(list.getSelectedValue().toString());
+			}
+		});
+		
 		getContentPane().setLayout(groupLayout);
 	}
 	
