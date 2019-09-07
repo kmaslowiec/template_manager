0f3dac664232a6f80215fe381b9228ff9d04599c
Kmaslowiec <kmaslowiec@gmail.com>
2019-09-07 03:08:42 +0200

    after refactor and moving to a new dir

--- src/main/java/com/github/kmaslowiec/template_manager/views/MainFrame.java
+++ src/main/java/com/github/kmaslowiec/template_manager/views/MainFrame.java
@@ -104,10 +104,8 @@ public class MainFrame extends JFrame {
 	private void createEvents() {
 		addTemplateEvent();
 		addElementEvent();
-<<<<<<< HEAD
 		templateClickedEvent();
-=======
->>>>>>> 3fadb8484b133fb76cb77ef9f2188ebd7f532917
+
 	}
 	
 	private void addTemplateEvent() {
@@ -127,7 +125,6 @@ public class MainFrame extends JFrame {
 		});
 	}
 	
-<<<<<<< HEAD
 	private void templateClickedEvent() {
 		list.addListSelectionListener(a->{
 			if(!a.getValueIsAdjusting()) {
@@ -135,9 +132,7 @@ public class MainFrame extends JFrame {
 			}
 		});
 	}
-	
-=======
->>>>>>> 3fadb8484b133fb76cb77ef9f2188ebd7f532917
+
 	private void setupDefaultJListRenderer(JList<Template> list) {
 		list.setCellRenderer(new DefaultListCellRenderer() {
 
