fa3a0ab1f52f06242db51315625fc7b69ddc08ca
Konrad Maslowiec <kmaslowiec@gmail.com>
2019-09-12 16:38:11 +0200

    many files start

--- src/main/java/com/github/kmaslowiec/template_manager/common/WordConverter.java
+++ src/main/java/com/github/kmaslowiec/template_manager/common/WordConverter.java
@@ -6,8 +6,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
